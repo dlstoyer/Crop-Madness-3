083a7b1d83d3fb4287cfc8f73e1957744a9a05aa
dlstoyer <dlstoyer@yahoo.com>
2016-12-10 15:45:51 -0500

    Timer and Finish Line
    Added an onscreen timer for scoring and built a goal/trigger for the
    'finish line'  Timer stops when you reach the goal.

---
+++ Assets/GameTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+    public Text TimerText;
+    private float startTime;
+    private bool finished = false;
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (finished)
+            return;
+        float t = Time.time - startTime;
+        string minutes = ((int) t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+        TimerText.text = minutes + ":" + seconds;
+
+	}
+    public void Finished()
+    {
+        finished = true;
+        TimerText.color = Color.yellow;
+    }
+
+}
