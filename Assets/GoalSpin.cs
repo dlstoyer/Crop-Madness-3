60efaa7603cd66e7f34902cc483c5c1c851245db
dlstoyer <dlstoyer@yahoo.com>
2016-12-10 23:35:39 -0500

    Rotating Goal finished and textured
    Late build

---
+++ Assets/GoalSpin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalSpin : MonoBehaviour {
+    public int SpinRate;
+    private bool positron;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        // ...also rotate around the World's Y axis
+        transform.Rotate(0, SpinRate, 0, Space.World);
+       
+
+    }
+}
